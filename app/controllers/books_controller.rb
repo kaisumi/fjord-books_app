fd3cf8d08f5cefdd4112dca5609fbd101beca32e
kaisumi <kaisumi@langue-lab.com>
2021-12-21 14:00:26 +0900

    「メールアドレスとパスワードでログインできる」まで完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,10 @@
 
 class BooksController < ApplicationController
   before_action :set_book, only: %i[show edit update destroy]
+  before_action do
+    I18n.locale = :ja
+  end
+  before_action :authenticate_user!
 
   # GET /books
   # GET /books.json
