96d31a08c5654ec4b2a8220a0e98fa5a6d3915b3
kaisumi <kaisumi@langue-lab.com>
2021-12-21 14:00:26 +0900

    「メールアドレスとパスワードでログインできる」まで完了

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
     config.public_file_server.headers = {
       'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
+    
   else
     config.action_controller.perform_caching = false
 
@@ -37,6 +38,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
