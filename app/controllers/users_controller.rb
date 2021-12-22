25dd63f0aa1b3b52c5a1430981c6eb9f90b71bf8
kaisumi <kaisumi@langue-lab.com>
2021-12-22 12:56:04 +0900

    認証はAppContで確認されるので不要な分岐を削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,9 @@
 class UsersController < ApplicationController
   def index
-    if user_signed_in?
-      @users = User.order(:id).page(params[:page])
-    else
-      redirect_to('/users/sign_in')
-    end
+    @users = User.order(:id).page(params[:page])
   end
 
   def show
-    if user_signed_in?
-      @user = User.find(params[:id])
-    else
-      redirect_to('/users/sign_in')
-    end
+    @user = User.find(params[:id])
   end
 end
