04236e7ffafe054886f1a5950cefafb43981945a
kaisumi <kaisumi@langue-lab.com>
2022-01-03 14:47:47 +0900

    バックスラッシュ削除、FollowerIDがない場合のエラーを回避

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -2,10 +2,14 @@
 
 class FollowsController < ApplicationController
   def show
-    @follows = \
-      Follower.find(User.find(params[:id]).follower.id).follows.map do |follow|
-        User.find(follow.user_id)
-      end
-    @follows = Kaminari.paginate_array(@follows).page(params[:page]) unless @follows.nil?
+    if Follower.exists?(user_id: params[:id])
+      @follows =
+        Follower.find(User.find(params[:id]).follower.id).follows.map do |follow|
+          User.find(follow.user_id)
+        end
+      @follows = Kaminari.paginate_array(@follows).page(params[:page])
+    else
+      @follows = nil
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @follower_number = @user.follows.count
-    @follow_number = Follower.find_by(user_id: params[:id]).follows.count
+    @follow_number = Follower.exists?(user_id: params[:id]) ? Follower.find_by(user_id: params[:id]).follows.count : 0
   end
 end
