cfc728ce96d33880ec26477a5e837436de4f4706
kaisumi <kaisumi@langue-lab.com>
2022-01-04 10:01:35 +0900

    詳細情報をusersテーブルに統合

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_23_020509) do
+ActiveRecord::Schema.define(version: 2022_01_03_232957) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -21,17 +21,6 @@ ActiveRecord::Schema.define(version: 2021_12_23_020509) do
     t.string "picture"
   end
 
-  create_table "user_details", force: :cascade do |t|
-    t.string "user_name"
-    t.string "postal_code"
-    t.text "address"
-    t.text "self_introduction"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
-    t.index ["user_id"], name: "index_user_details_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -40,9 +29,12 @@ ActiveRecord::Schema.define(version: 2021_12_23_020509) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "user_name"
+    t.string "postal_code"
+    t.text "address"
+    t.text "self_introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "user_details", "users"
 end
