895c38e52d119a4e67497d6d9649b855a1fba783
kaisumi <kaisumi@langue-lab.com>
2022-01-05 13:24:40 +0900

    scoped_viewsでviewを取得するように修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'books#index'
-  devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
+  devise_for :users
   resources :books
   resources :users, :only => [:index, :show]
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
