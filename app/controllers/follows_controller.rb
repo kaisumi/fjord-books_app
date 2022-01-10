fd3ab3e34592583198b296c5e4c73fd56496dd0a
kaisumi <kaisumi@langue-lab.com>
2022-01-10 14:44:27 +0900

    Controllerをfollowsに統合

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
 class FollowsController < ApplicationController
-  def show
+  def create
+    current_user.follows_as_following.create(following_id: params[:id])
+    redirect_to user_path
+  end
+
+  def show_followers
+    @followers = User.find(params[:id]).followers
+    @followers = Kaminari.paginate_array(@followers).page(params[:page]) unless @followers.nil?
+  end
+
+  def show_followings
     @follows = User.find(params[:id]).followings
     @follows = Kaminari.paginate_array(@follows).page(params[:page])
+  end
+
+  def destroy
+    @follow = current_user.follows_as_following.find_by(following_id: params[:id])
+    @follow.destroy
+    redirect_to user_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   root to: 'books#index'
   resources :books
   resources :users, only: %i(index show)
-  get '/users/:id/follows', to: 'follows#show', as: 'follow_list'
-  get '/users/:id/followers', to: 'followers#show', as: 'follower_list'
-  post '/users/:id/followers', to: 'followers#create', as: 'follow'
-  delete '/users/:user_id/followers/:id', to: 'followers#destroy', as: 'unfollow'
+  get '/users/:id/follows', to: 'follows#show_followings', as: 'follow_list'
+  get '/users/:id/followers', to: 'follows#show_followers', as: 'follower_list'
+  post '/users/:id/followers', to: 'follows#create', as: 'follow'
+  delete '/users/:user_id/followers/:id', to: 'follows#destroy', as: 'unfollow'
 end
