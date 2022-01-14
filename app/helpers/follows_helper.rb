4fc96ba70a41ff915b5d27e30c6a2556a932241b
kaisumi <kaisumi@langue-lab.com>
2022-01-14 16:58:27 +0900

    follower?をUserに移動し、following?に変更

--- app/helpers/follows_helper.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module FollowsHelper
-  def follower?(follower_id:, following_id:)
-    follower = User.find(follower_id)
-    follower.followings.exists?(id: following_id)
-  end
-end
