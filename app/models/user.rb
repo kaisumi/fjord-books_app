315263daccb04a02faf1845d516077d098ee4bcb
kaisumi <kaisumi@langue-lab.com>
2022-01-18 21:04:34 +0900

    GitHub認証機能を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,21 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[github]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name   # assuming the user model has a name
+      # user.image = auth.info.image # assuming the user model has an image
+    end
+  end
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      user.email = session['devise.github_data']['email'] if user.email.blank? && !session['devise.github_data']['email'].nil?
+    end
+  end
 end
